96bbdb1fb7d896c446f255b01bb9a3c1e2e44494
Karsten Gresch <git-2015@by-doing.org>
2015-09-19 21:32:48 +0200

    Added older Auto Layout course.

---
+++ AutoLayoutCourse01/AutoLayoutCourse01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutCourse01
+//
+//  Created by Karsten Gresch on 11.05.15.
+//  Copyright (c) 2015 Closure One. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
