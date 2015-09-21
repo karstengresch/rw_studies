1c72aada955e6579aa4fa48d40bb1d48e4e09811
Karsten Gresch <git-2015@by-doing.org>
2015-09-21 22:42:01 +0200

    Fixed the round arc.

--- Countidon/Countidon/CounterView.swift
+++ Countidon/Countidon/CounterView.swift
@@ -26,7 +26,7 @@ override func drawRect(rect: CGRect) {
   // center
   let center = CGPoint(x: bounds.width/2, y: bounds.height/2)
   let radius: CGFloat = max(bounds.width/2, bounds.height/2)
-  let arcWidth: CGFloat = 76
+  let arcWidth: CGFloat = 50
   let startAngle: CGFloat = 3 * π / 4
   let endAngle: CGFloat = π / 4
   
@@ -38,15 +38,15 @@ override func drawRect(rect: CGRect) {
   
   // Outline
   let angleDifference: CGFloat = 2 * π - startAngle + endAngle
-  let arclengthPerGlass = angleDifference / CGFloat(NumberOfGlasses)
-  let outlineEndAngle = arclengthPerGlass * CGFloat(counter) + startAngle
+  let arcLengthPerGlass = angleDifference / CGFloat(NumberOfGlasses)
+  let outlineEndAngle = arcLengthPerGlass * CGFloat(counter) + startAngle
   
-  let outlinePath = UIBezierPath(arcCenter: center, radius: bounds.width/2 - 2.5, startAngle: startAngle, endAngle: outlineEndAngle, clockwise: true)
-  outlinePath.addArcWithCenter(center, radius: bounds.width/2 - arcWidth + 2.5, startAngle: outlineEndAngle, endAngle: startAngle, clockwise: false)
+  let outlinePath = UIBezierPath(arcCenter: center, radius: bounds.width/2.79, startAngle: startAngle, endAngle: outlineEndAngle, clockwise: true)
+  outlinePath.addArcWithCenter(center, radius: bounds.width/2 - arcWidth*1.69 + 2.5, startAngle: outlineEndAngle, endAngle: startAngle, clockwise: false)
 
   outlinePath.closePath()
   outlineColor.setStroke()
-  outlinePath.lineWidth = 5.0
+  outlinePath.lineWidth = 3.0
   outlinePath.stroke()
 
   
