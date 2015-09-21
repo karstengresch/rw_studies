e79319ec19e025a19f27d4f4fb1c8baa45902c09
Karsten Gresch <git-2015@by-doing.org>
2015-09-21 16:45:48 +0200

    Added the glasses arc. Some fixes were needed...

--- Countidon/Countidon/CounterView.swift
+++ Countidon/Countidon/CounterView.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
+let NumberOfGlasses = 8
+let π: CGFloat = CGFloat(M_PI)
+
 @IBDesignable
-class CounterView: UIButton {
+class CounterView: UIView {
   
   @IBInspectable var counter: Int = 5
   @IBInspectable var outlineColor: UIColor = UIColor.blueColor()
@@ -20,6 +23,19 @@ class CounterView: UIButton {
     // An empty implementation adversely affects performance during animation.
     */
 override func drawRect(rect: CGRect) {
+  // center
+  let center = CGPoint(x: bounds.width/2, y: bounds.height/2)
+  let radius: CGFloat = max(bounds.width/2, bounds.height/2)
+  let arcWidth: CGFloat = 56
+  let startAngle: CGFloat = 3 * π / 4
+  let endAngle: CGFloat = π / 4
+  
+  let path = UIBezierPath(arcCenter: center, radius: radius/2, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+  
+  path.lineWidth = arcWidth
+  counterColor.setStroke()
+  path.stroke()
+  
       
     }
 
