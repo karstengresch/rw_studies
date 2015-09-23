85eaa0de859e46ab71e10c36365236f3ae312e20
Karsten Gresch <git-2015@by-doing.org>
2015-09-23 07:57:28 +0200

    Tap Gesture implemented

--- Countidon/Countidon/ViewController.swift
+++ Countidon/Countidon/ViewController.swift
@@ -10,28 +10,32 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
+  
   @IBOutlet weak var counterView: CounterView?
   @IBOutlet weak var counterLabel: UILabel?
   @IBOutlet weak var containerView: UIView?
   @IBOutlet weak var graphView: GraphView?
   
-
+  var isGraphViewShowing = false
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     counterLabel?.text = String(counterView?.counter)
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
   
+  // MARK: Actions
+  
   @IBAction func pushButtonPressed(button: PushButtonView) {
     if button.isAddButton {
       if counterView?.counter <= NumberOfGlasses {
-      counterView?.counter++
+        counterView?.counter++
       }
     } else {
       if counterView?.counter > 0 {
@@ -45,8 +49,24 @@ class ViewController: UIViewController {
       counterLabel!.text = String(currentCounter)
     }
   }
-
   
-
+  @IBAction func counterViewTap(gestureRecognizer: UITapGestureRecognizer?) {
+    
+    if let counterView = counterView, let graphView = graphView
+    {
+      if (isGraphViewShowing) {
+        UIView.transitionFromView(graphView, toView: counterView, duration: 1.0, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+        
+      } else {
+        UIView.transitionFromView(counterView, toView: graphView, duration: 1.0, options: [UIViewAnimationOptions.TransitionFlipFromRight, UIViewAnimationOptions.ShowHideTransitionViews], completion: nil)
+      }
+      isGraphViewShowing = !isGraphViewShowing
+    }
+    
+    
+  }
+  
+  
+  
 }
 
