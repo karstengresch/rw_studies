f36b84f3230413bc0b201a2a98d3cbcbe74ee397
Karsten Gresch <git-2015@by-doing.org>
2015-09-23 07:18:58 +0200

    Added empty GraphView

---
+++ Countidon/Countidon/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Countidon
+//
+//  Created by Karsten Gresch on 23.09.15.
+//  Copyright © 2015 Closure One. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
