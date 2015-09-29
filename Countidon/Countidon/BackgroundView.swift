eac5084dbaca098a02bfe44ad5fbf232904e587a
Karsten Gresch <git-2015@by-doing.org>
2015-09-29 20:58:19 +0200

    Added BG view 4 pattern BG

---
+++ Countidon/Countidon/BackgroundView.swift
@@ -0,0 +1,19 @@
+//
+//  BackgroundView.swift
+//  Countidon
+//
+//  Created by Karsten Gresch on 29.09.15.
+//  Copyright © 2015 Closure One. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundView: UIView {
+
+
+    override func drawRect(rect: CGRect) {
+
+    }
+
+
+}
