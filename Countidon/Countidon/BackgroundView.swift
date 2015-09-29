f53713dd27574f9cf43e72177ff31fef0cfbb417
Karsten Gresch <git-2015@by-doing.org>
2015-09-29 21:50:02 +0200

    Background pattern impl

--- Countidon/Countidon/BackgroundView.swift
+++ Countidon/Countidon/BackgroundView.swift
@@ -19,9 +19,26 @@ class BackgroundView: UIView {
   
     override func drawRect(rect: CGRect) {
       let context = UIGraphicsGetCurrentContext()
-      
+      // NB - don't confuse w/ CGContextSetFillColor - UnsafePointer stuff'd b ur rewards
       CGContextSetFillColorWithColor(context, darkColor.CGColor)
       CGContextFillRect(context, rect)
+      // pattern
+      let drawSize = CGSize(width: patternSize, height: patternSize)
+      let trianglePath = UIBezierPath()
+      trianglePath.moveToPoint(CGPoint(x: drawSize.width/2, y: 0))
+      trianglePath.addLineToPoint(CGPoint(x: 0, y: drawSize.height/2))
+      trianglePath.addLineToPoint(CGPoint(x: drawSize.width, y: drawSize.height/2))
+      
+      trianglePath.moveToPoint(CGPoint(x: drawSize.width/2, y: 0))
+      trianglePath.addLineToPoint(CGPoint(x: drawSize.width/2, y: drawSize.height))
+      trianglePath.addLineToPoint(CGPoint(x: 0, y: drawSize.height))
+      
+      trianglePath.moveToPoint(CGPoint(x: drawSize.width, y: drawSize.height/2))
+      trianglePath.addLineToPoint(CGPoint(x: drawSize.width/2, y: drawSize.height))
+      trianglePath.addLineToPoint(CGPoint(x: drawSize.width, y: drawSize.height))
+      
+      lightColor.setFill()
+      trianglePath.fill()
 
     }
 
