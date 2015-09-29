09f3b890014f76c71902de601fbe70db5d9c05f9
Karsten Gresch <git-2015@by-doing.org>
2015-09-29 21:30:11 +0200

    Base draw() impl

--- Countidon/Countidon/BackgroundView.swift
+++ Countidon/Countidon/BackgroundView.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
+@IBDesignable
+
 class BackgroundView: UIView {
 
-
+  @IBInspectable var lightColor: UIColor = UIColor.orangeColor()
+  @IBInspectable var darkColor: UIColor = UIColor.darkGrayColor()
+  @IBInspectable var patternSize: CGFloat = 200
+  
+  
     override func drawRect(rect: CGRect) {
+      let context = UIGraphicsGetCurrentContext()
+      
+      CGContextSetFillColorWithColor(context, darkColor.CGColor)
+      CGContextFillRect(context, rect)
 
     }
 
