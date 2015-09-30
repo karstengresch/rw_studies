65f852ddc8e58e6493ecce9310c7f784a2e69847
Karsten Gresch <git-2015@by-doing.org>
2015-09-30 22:11:41 +0200

    Mediallion as rectangle w/ gradient

--- Countidon/Countidon/MedalDrawing.playground/Contents.swift
+++ Countidon/Countidon/MedalDrawing.playground/Contents.swift
@@ -13,6 +13,7 @@ let context = UIGraphicsGetCurrentContext()
 let darkGoldColor = UIColor(red: 0.6, green: 0.5, blue: 0.15, alpha: 1.0)
 let midGoldColor = UIColor(red: 0.86, green: 0.73, blue: 0.3, alpha: 1.0)
 let lightColdColor = UIColor(red: 1.0, green: 0.98, blue: 0.9, alpha: 1.0)
+
 // lower ribbon
 var lowerRibbonPath = UIBezierPath()
 lowerRibbonPath.moveToPoint(CGPointMake(0, 0))
@@ -20,8 +21,28 @@ lowerRibbonPath.addLineToPoint(CGPointMake(40, 0))
 lowerRibbonPath.addLineToPoint(CGPointMake(78, 70))
 lowerRibbonPath.addLineToPoint(CGPointMake(38, 70))
 lowerRibbonPath.closePath()
-UIColor.redColor().setFill()
+UIColor(red: 0.99, green: 0.4, blue: 0.4, alpha: 0.8).setFill()
 lowerRibbonPath.fill()
+
+// clasp
+// o what punny this is!
+var claspPath = UIBezierPath(roundedRect: CGRectMake(36, 62, 43, 20), cornerRadius: 5)
+claspPath.lineWidth = 5
+darkGoldColor.setStroke()
+claspPath.stroke()
+
+
+
+
+
+var medallionPath = UIBezierPath(ovalInRect: CGRect(origin: CGPointMake(8, 72), size: CGSizeMake(100, 100)))
+// CGContextSaveGState(context)
+// medallionPath.addClip()
+let gradient = CGGradientCreateWithColors(CGColorSpaceCreateDeviceRGB(), [darkGoldColor.CGColor, midGoldColor.CGColor, lightColdColor.CGColor], [0, 0.51, 1])
+CGContextDrawLinearGradient(context, gradient, CGPointMake(40, 40), CGPointMake(40, 162),  CGGradientDrawingOptions())
+// CGContextSaveGState(context)
+
+
 
 // keep @ end!
 let image = UIGraphicsGetImageFromCurrentImageContext()
