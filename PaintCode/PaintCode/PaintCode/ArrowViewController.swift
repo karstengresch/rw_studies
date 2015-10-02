64990b97c74f10eb48696b69c9ce64ba976d9aef
Karsten Gresch <git-2015@by-doing.org>
2015-10-02 09:22:42 +0200

    Added the starter project, converted it to Swift 2

---
+++ PaintCode/PaintCode/PaintCode/ArrowViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ArrowViewController.swift
+//  PaintCode
+//
+//  Created by Felipe Laso Marsetti on 1/15/15.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArrowViewController: UIViewController {
+}
---
+++ PaintCode/PaintCode/PaintCode/ControlViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ControlViewController.swift
+//  PaintCode
+//
+//  Created by Felipe Laso Marsetti on 1/15/15.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ControlViewcontroller: UIViewController {
+}
+
