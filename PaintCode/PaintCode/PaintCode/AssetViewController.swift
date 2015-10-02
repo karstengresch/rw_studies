47847baf37d23a2e0c08fe87387c7b509cf0da8c
Karsten Gresch <git-2015@by-doing.org>
2015-10-02 22:02:19 +0200

    Trying to start/stop the animation, not w/ the desired effect.

--- PaintCode/PaintCode/PaintCode/AssetViewController.swift
+++ PaintCode/PaintCode/PaintCode/AssetViewController.swift
@@ -13,21 +13,53 @@ class AssetViewController: UIViewController {
 
 
   @IBOutlet weak var stopWatchHandView: UIView?
+  var isStopwatchRunning = false
   
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
-    rotateStopWatchHand()
+    // rotateStopWatchHand()
     
   }
   
-  func rotateStopWatchHand() {
-    UIView.animateWithDuration(1.0, delay: 0, options: .CurveLinear, animations:
-      { self.stopWatchHandView!.transform = CGAffineTransformRotate(self.stopWatchHandView!.transform, CGFloat(M_PI/15))
+  @IBAction func rotateStopWatchHand() {
+    print("TAP!")
+
+    if self.isStopwatchRunning == false {
+      print("START")
+    print("rotateStopWatchHand - running? \(isStopwatchRunning) ")
+    animateStopwatchHand()
+      
+
+      
+    } else {
+      print("STOP")
+      self.stopWatchHandView!.layer.removeAllAnimations()
+      self.isStopwatchRunning = false
+    }
+    
+    
+  }
+
+  func animateStopwatchHand() {
+    self.isStopwatchRunning = true
+    
+    UIView.animateWithDuration(15.0, delay: 0, options: .CurveLinear, animations:
+      {
+        print("Animation closure")
+        self.stopWatchHandView!.transform = CGAffineTransformRotate(self.stopWatchHandView!.transform, CGFloat(M_PI_4 * -45))
+        self.isStopwatchRunning = true
       }, completion:
-      { finished in self.rotateStopWatchHand()
-      })
+      { finished in
+        // self.animateStopwatchHand()
+        print("Animation completion closure")
+        self.isStopwatchRunning = true
+    })
+    
+
   }
   
 }
+
+
 
