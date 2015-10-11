f61d4dad70d3b330dd0235b74ae3c9e2fb0a5032
Karsten Gresch <git-2015@by-doing.org>
2015-10-11 11:26:03 +0200

    Added and set up RoundedView

---
+++ RWBooks/RWBooks/RoundedView.swift
@@ -0,0 +1,15 @@
+//
+//  RoundedView.swift
+//  RWBooks
+//
+//  Created by Karsten Gresch on 11.10.15.
+//  Copyright © 2015 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedView: UIView {
+  
+  
+}
