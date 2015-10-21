e6c6d74a0c847b8586e1c13753f1f664e453e44c
Karsten Gresch <git-2015@by-doing.org>
2015-10-22 01:01:06 +0200

    Rectangle animation broken: 'fatal error: use of unimplemented initializer 'init(layer:)' for class 'SBLoader.RectangleLayer'. Need to check later.

--- SBLoader - Starter/SBLoader/RectangleLayer.swift
+++ SBLoader - Starter/SBLoader/RectangleLayer.swift
@@ -33,6 +33,11 @@ class RectangleLayer: CAShapeLayer {
   }
   
   func animateStrokeWithColor(color: UIColor) {
-    
+    strokeColor = color.CGColor
+    let strokeAnimation: CABasicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+    strokeAnimation.fromValue = 0.0
+    strokeAnimation.toValue = 1.0
+    strokeAnimation.duration = 0.4
+    addAnimation(strokeAnimation, forKey: nil)
   }
 }
