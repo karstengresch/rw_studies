d3aee6e80b85631614f36fc5958cbb9d51ec72a6
Karsten Gresch <git-2015@by-doing.org>
2015-10-25 00:03:20 +0100

    Expand view prep in ViewController

--- SBLoader - Starter/SBLoader/ViewController.swift
+++ SBLoader - Starter/SBLoader/ViewController.swift
@@ -39,6 +39,22 @@ class ViewController: UIViewController, HolderViewDelegate {
   }
   
   func animateLabel() {
+    
+    holderView.removeFromSuperview()
+    view.backgroundColor = Colors.blue
+    
+    var label = UILabel(frame: view.frame)
+    label.textColor = Colors.white
+    label.font = UIFont(name: "HelveticaNeue-Thin", size: 170.0)
+    label.textAlignment = NSTextAlignment.Center
+    label.text = "G"
+    label.transform = CGAffineTransformScale(label.transform, 0.25, 0.25)
+    view.addSubview(label)
+    
+    UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveEaseInOut, animations: ( { label.transform = CGAffineTransformScale(label.transform, 4.0, 4.0) } ), completion: { finished in self.addButton() })
+    
+    
+    
 
   }
   
