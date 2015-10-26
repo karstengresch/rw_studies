05ffe1654368619e8c59d2d5853f151cb68b74a6
Karsten Gresch <git-2015@by-doing.org>
2015-10-26 11:14:58 +0000

    Fixed submodule problem.

---
+++ Knob/KnobDemo/ApplicationDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  ApplicationDelegate.swift
+//  KnobDemo
+//
+//  Created by Mikael Konutgan on 05/09/14.
+//  Copyright (c) 2014 Mikael Konutgan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
---
+++ Knob/KnobDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KnobDemo
+//
+//  Created by Mikael Konutgan on 05/09/14.
+//  Copyright (c) 2014 Mikael Konutgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var knobPlaceholder: UIView!
+    @IBOutlet var valueLabel: UILabel!
+    @IBOutlet var valueSlider: UISlider!
+    @IBOutlet var animateSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func sliderValueChanged(slider: UISlider) {
+    }
+    
+    @IBAction func randomButtonTouched(button: UIButton) {
+    }
+}
