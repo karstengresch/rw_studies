7be25327506c8373e2b1e68e0ee0a6f8f24e8535
Karsten Gresch <git-2015@by-doing.org>
2015-10-29 23:37:23 +0100

    Knop API

--- Knob/KnobDemo/Knob.swift
+++ Knob/KnobDemo/Knob.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 public class Knob: UIControl {
+  private var backingValue: Float = 0.0
+  
+  
+  public var value: Float {
+    get { return backingValue }
+    set { setValue(newValue, animated: false) }
+  }
+  
+  public func setValue(value: Float, animated: Bool) {
+    if value != backingValue {
+      backingValue = min(maximumValue, max(minimumValue, value))
+    }
+  }
+  
+  /** */
+  public var minimumValue: Float = 0.0
+  /** */
+  public var maximumValue: Float = 1.0
+  /** */
+  public var continuous: Bool = true
+  
 
   public override init(frame: CGRect) {
     super.init(frame: frame)
