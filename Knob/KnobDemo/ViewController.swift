8d907d9a67548512e399ec3b187f6197f1211296
Karsten Gresch <git-2015@by-doing.org>
2015-10-29 23:24:23 +0100

    added Knob class

--- Knob/KnobDemo/ViewController.swift
+++ Knob/KnobDemo/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
     @IBAction func sliderValueChanged(slider: UISlider) {
