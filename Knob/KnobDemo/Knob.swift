b8b962209140b94e8894d46053f10c0d3f0a4d18
Karsten Gresch <git-2015@by-doing.org>
2015-10-29 23:30:12 +0100

    Fixed file problem w/ Knob.swift

--- Knob/KnobDemo/Knob.swift
+++ Knob/KnobDemo/Knob.swift
@@ -1,7 +1,15 @@
+//
+//  Knobb.swift
+//  KnobDemo
+//
+//  Created by Karsten Gresch on 29.10.15.
+//  Copyright © 2015 Mikael Konutgan. All rights reserved.
+//
+
 import UIKit
 
 public class Knob: UIControl {
-  
+
   public override init(frame: CGRect) {
     super.init(frame: frame)
     backgroundColor = tintColor
@@ -11,8 +19,6 @@ public class Knob: UIControl {
   public required init(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented!")
   }
-  
-  
 
-  
+
 }
