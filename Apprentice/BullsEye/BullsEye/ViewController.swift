bc0c4a9686f9491e0c3896352bad88215df1fbe2
Karsten Gresch <git-2015@by-doing.org>
2015-11-22 12:45:35 +0000

    Slider adjustments

--- Apprentice/BullsEye/BullsEye/ViewController.swift
+++ Apprentice/BullsEye/BullsEye/ViewController.swift
@@ -21,9 +21,28 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+    slider?.setThumbImage(thumbImageNormal, forState: .Normal)
+    let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+    slider?.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+    
+    let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+    
+    if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+      let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+      slider?.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+    }
+
+    if let trackRightImage = UIImage(named: "SliderTrackRight") {
+      let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+      slider?.setMinimumTrackImage(trackRightResizable, forState: .Normal)
+    }
+    
+    
     startNewRound()
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
@@ -53,7 +72,7 @@ class ViewController: UIViewController {
   @IBAction func showAlert() {
     let difference = abs(targetValue - currentValue)
     var points = 100 - difference
-
+    
     let title: String
     if difference == 0 {
       title = "Perfect match!"
@@ -95,6 +114,6 @@ class ViewController: UIViewController {
     updateLabels()
     
   }
-
+  
 }
 
