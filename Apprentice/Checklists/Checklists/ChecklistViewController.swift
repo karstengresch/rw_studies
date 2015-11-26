fb99abc9049470622fe1c740d837176e17e77eb5
Karsten Gresch <git-2015@by-doing.org>
2015-11-26 22:48:19 +0100

    Now w/ 100 rows. Completely forgot the tag ID trick, so working through was worth it already :)

--- Apprentice/Checklists/Checklists/ChecklistViewController.swift
+++ Apprentice/Checklists/Checklists/ChecklistViewController.swift
@@ -21,32 +21,33 @@ class ChecklistViewController: UITableViewController {
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5
+    return 100
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
     let label = cell.viewWithTag(1000) as? UILabel
     
-switch indexPath.row {
+  switch indexPath.row % 5 {
   
-case (0):
-  label?.text = "Walk the bird."
-  
-case (1):
-  label?.text = "Brush your hair."
-  
-case (2):
-  label?.text = "Learn singing."
-  
-case (3):
-  label?.text = "Yoga practice."
-  
-case (4):
-  label?.text = "Eat Filet Wellington."
-  
-default:
-  label?.text = "Check the code."
+    case (0):
+      label?.text = "Walk the bird."
+      
+    case (1):
+      label?.text = "Brush your hair."
+      
+    case (2):
+      label?.text = "Learn singing."
+      
+    case (3):
+      label?.text = "Yoga practice."
+      
+    case (4):
+      label?.text = "Eat Filet Wellington."
+      
+    default:
+      label?.text = "Check the code."
+    
     }
     
     return cell
