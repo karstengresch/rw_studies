46825a4f590274af4ae4c62ac0b9619216306832
Karsten Gresch <git-2015@by-doing.org>
2015-11-26 22:39:47 +0100

    Added multiple tabels

--- Apprentice/Checklists/Checklists/ChecklistViewController.swift
+++ Apprentice/Checklists/Checklists/ChecklistViewController.swift
@@ -21,11 +21,32 @@ class ChecklistViewController: UITableViewController {
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return 5
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+    let label = cell.viewWithTag(1000) as? UILabel
+    
+switch indexPath.row {
+  
+case (0):
+  label?.text = ""
+  
+case (1):
+  label?.text = ""
+  
+case (2):
+  label?.text = ""
+  
+case (3):
+  label?.text = ""
+  
+case (4):
+  label?.text = ""
+  
+    }
+    
     return cell
   }
   
