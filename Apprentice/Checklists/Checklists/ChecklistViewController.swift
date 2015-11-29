74aa7e2202deb2e0e4e4864afd4712fa1cb19650
Karsten Gresch <git-2015@by-doing.org>
2015-11-29 23:09:10 +0100

    Default checkers

--- Apprentice/Checklists/Checklists/ChecklistViewController.swift
+++ Apprentice/Checklists/Checklists/ChecklistViewController.swift
@@ -69,6 +69,32 @@ class ChecklistViewController: UITableViewController {
     return cell
   }
   
+    // MARK: Content related methods
+  func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+    var isChecked = false
+    
+    switch indexPath.row {
+    case (0):
+      isChecked = row0checked
+    case (1):
+      isChecked = row1checked
+    case (2):
+      isChecked = row2checked
+    case (3):
+      isChecked = row3checked
+    case (4):
+      isChecked = row4checked
+    default:
+      isChecked = true
+    }
+    
+    if isChecked {
+      cell.accessoryType = .Checkmark
+    } else {
+      cell.accessoryType = .None
+    }
+  }
+  
   
     // MARK: Table delegate methods
   override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
