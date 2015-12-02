6fbb6ce1f04f8b733f50b054bbd644ebfafc7157
Karsten Gresch <git-2015@by-doing.org>
2015-12-02 20:45:38 +0100

    Partial dummy methods (only cancel is useful).

--- Apprentice/Checklists/Checklists/AddItemViewController.swift
+++ Apprentice/Checklists/Checklists/AddItemViewController.swift
@@ -11,5 +11,13 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
   
+  @IBAction func cancel() {
+   dismissViewControllerAnimated(true, completion: nil)
+    
+  }
+  
+  @IBAction func done() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
   
 }
