e38e4b0458a57107f6754fdebf12ea715de1dd2d
Karsten Gresch <git-2015@by-doing.org>
2015-12-02 20:39:51 +0100

    Separate VC for the new table view

---
+++ Apprentice/Checklists/Checklists/AddItemViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Karsten Gresch on 02.12.15.
+//  Copyright © 2015 Closure One. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddItemViewController: UITableViewController {
+  
+  
+}
