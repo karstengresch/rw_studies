0ad184d9da6c4ccb95f9d2f087a8c56c90524a7f
Karsten Gresch <git-2015@by-doing.org>
2015-12-06 20:49:56 +0100

    Removed the old add item methods

--- Apprentice/Checklists/Checklists/ChecklistViewController.swift
+++ Apprentice/Checklists/Checklists/ChecklistViewController.swift
@@ -135,21 +135,7 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
   }
   
   // MARK: Action handlers
-  @IBAction func addItem() {
-    let newRowIndex = checkListItems.count
-    
-    let checklistItem = ChecklistItem()
-    checklistItem.text = "New to do"
-    checklistItem.checked = true
-    
-    checkListItems.append(checklistItem)
-    
-    let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
-    let indexPaths = [indexPath, indexPath, indexPath]
-    
-    tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
-    
-  }
+
 
 }
 
