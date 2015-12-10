ba86b241204e67cda97545b1bdc9c5ec3653ed2b
Karsten Gresch <git-2015@by-doing.org>
2015-12-10 22:05:49 +0100

    Decoding

--- Apprentice/Checklists/Checklists/ChecklistItem.swift
+++ Apprentice/Checklists/Checklists/ChecklistItem.swift
@@ -28,6 +28,10 @@ class ChecklistItem: NSObject, NSCoding {
   }
   
   required init?(coder aDecoder: NSCoder) {
+    
+    text = aDecoder.decodeObjectForKey("Text") as! String
+    checked = aDecoder.decodeBoolForKey("Checked")
+    
     super.init()
   }
   
