cbd9ba26004b733756d5cdffe15daf9393ff2a8e
Karsten Gresch <git-2015@by-doing.org>
2015-12-25 23:23:00 +0100

    Finally working persistence.

--- Apprentice/Checklists/Checklists/AllListsTableViewController.swift
+++ Apprentice/Checklists/Checklists/AllListsTableViewController.swift
@@ -137,24 +137,24 @@ class AllListsTableViewController: UITableViewController, ListDetailViewControll
   }
   
   func dataFilePath() -> String {
-  return (documentsDirectory() as NSString).stringByAppendingPathComponent("Checklist.plist")
+  return (documentsDirectory() as NSString).stringByAppendingPathComponent("Checklists.plist")
   }
   
   func saveChecklists() {
-  let data = NSMutableData()
-  let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
-  archiver.encodeObject(checklists, forKey: "Checklists")
-  archiver.finishEncoding()
-  data.writeToFile(dataFilePath(), atomically: true)
+    let data = NSMutableData()
+    let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+    archiver.encodeObject(checklists, forKey: "Checklists")
+    archiver.finishEncoding()
+    data.writeToFile(dataFilePath(), atomically: true)
   }
   
   func loadChecklists() {
-  let path = dataFilePath()
-  if NSFileManager.defaultManager().fileExistsAtPath(path) {
-  if let data = NSData(contentsOfFile: path) {
-  let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
-  checklists = unarchiver.decodeObjectForKey("Checklists") as! [Checklist]
-  unarchiver.finishDecoding()
+    let path = dataFilePath()
+    if NSFileManager.defaultManager().fileExistsAtPath(path) {
+      if let data = NSData(contentsOfFile: path) {
+        let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+        checklists = unarchiver.decodeObjectForKey("Checklists") as! [Checklist]
+        unarchiver.finishDecoding()
   }
   }
   
