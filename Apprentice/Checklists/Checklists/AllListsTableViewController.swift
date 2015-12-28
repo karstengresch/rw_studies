b7e557fbd76b9f0a2170d6220d4ffcb8dc759825
Karsten Gresch <git-2015@by-doing.org>
2015-12-28 17:35:48 +0100

    Moving the plist storage methods to DataModel

--- Apprentice/Checklists/Checklists/AllListsTableViewController.swift
+++ Apprentice/Checklists/Checklists/AllListsTableViewController.swift
@@ -129,35 +129,4 @@ class AllListsTableViewController: UITableViewController, ListDetailViewControll
       
     }
   
-  
-  // MARK: File related
-  func documentsDirectory() -> String {
-  let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
-  return paths[0]
-  }
-  
-  func dataFilePath() -> String {
-  return (documentsDirectory() as NSString).stringByAppendingPathComponent("Checklists.plist")
-  }
-  
-  func saveChecklists() {
-    let data = NSMutableData()
-    let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
-    archiver.encodeObject(checklists, forKey: "Checklists")
-    archiver.finishEncoding()
-    data.writeToFile(dataFilePath(), atomically: true)
-  }
-  
-  func loadChecklists() {
-    let path = dataFilePath()
-    if NSFileManager.defaultManager().fileExistsAtPath(path) {
-      if let data = NSData(contentsOfFile: path) {
-        let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
-        checklists = unarchiver.decodeObjectForKey("Checklists") as! [Checklist]
-        unarchiver.finishDecoding()
-  }
-  }
-  
-  }
-  
 }
--- Apprentice/Checklists/Checklists/DataModel.swift
+++ Apprentice/Checklists/Checklists/DataModel.swift
@@ -11,5 +11,34 @@ import Foundation
 class DataModel {
   var checklists = [Checklist]()
   
+  // MARK: File related
+  func documentsDirectory() -> String {
+    let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+    return paths[0]
+  }
+  
+  func dataFilePath() -> String {
+    return (documentsDirectory() as NSString).stringByAppendingPathComponent("Checklists.plist")
+  }
+  
+  func saveChecklists() {
+    let data = NSMutableData()
+    let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+    archiver.encodeObject(checklists, forKey: "Checklists")
+    archiver.finishEncoding()
+    data.writeToFile(dataFilePath(), atomically: true)
+  }
+  
+  func loadChecklists() {
+    let path = dataFilePath()
+    if NSFileManager.defaultManager().fileExistsAtPath(path) {
+      if let data = NSData(contentsOfFile: path) {
+        let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+        checklists = unarchiver.decodeObjectForKey("Checklists") as! [Checklist]
+        unarchiver.finishDecoding()
+      }
+    }
+  }
+  
   
 }
