079160b209b810132c3dc6259815048f8fe74845
Karsten Gresch <git-2015@by-doing.org>
2016-01-23 21:23:50 +0100

    Added static icon categories

--- Apprentice/Checklists/Checklists/IconPickerViewController.swift
+++ Apprentice/Checklists/Checklists/IconPickerViewController.swift
@@ -16,4 +16,17 @@ protocol IconPickerViewControllerDelegate: class {
 class IconPickerViewController: UITableViewController {
   weak var delegate: IconPickerViewControllerDelegate?
   
+  let icons = [ "No Icon",
+                "Appointments",
+                "Birthdays",
+                "Chores",
+                "Drinks",
+                "Folder",
+                "Groceries",
+                "Inbox",
+                "Photos",
+                "Trips" ]
+  
+  
+  
 }
