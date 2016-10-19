bed7ecd57a413e69d34ce1ba136a3425c899871b
Karsten Gresch <git-2015@by-doing.org>
2016-10-19 23:00:28 +0200

    numbersOfRows

--- Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
@@ -20,6 +20,11 @@ class Checklist10ViewController: UITableViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
     return cell
