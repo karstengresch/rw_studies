34ab00a9225d47cc6beaead2c2f08a13391c082d
Karsten Gresch <git-2015@by-doing.org>
2016-10-19 23:11:23 +0200

    Label text, more secure

--- Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
@@ -27,6 +27,21 @@ class Checklist10ViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+    
+    if let label = cell.viewWithTag(1000) as! UILabel? {
+      if indexPath.row == 0 {
+        label.text = "Walk the dog"
+      } else if indexPath.row == 1 {
+        label.text = "Brush my teeth"
+      } else if indexPath.row == 2 {
+        label.text = "Learn iOS development"
+      } else if indexPath.row == 3 {
+        label.text = "Soccer practice"
+      } else if indexPath.row == 4 {
+        label.text = "Eat ice cream"
+      }
+    }
+    
     return cell
   }
 
