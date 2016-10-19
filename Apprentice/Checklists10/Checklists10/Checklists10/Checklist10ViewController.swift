9224fc7ae7bf5b9fa6713bb57bb47cf33d0c7857
Karsten Gresch <git-2015@by-doing.org>
2016-10-19 23:06:25 +0200

    5 rowas

--- Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
@@ -22,7 +22,7 @@ class Checklist10ViewController: UITableViewController {
   
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return 5
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
