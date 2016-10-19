1fc5b141c40d512f14c6d4e116ba1b8323731114
Karsten Gresch <git-2015@by-doing.org>
2016-10-19 23:17:02 +0200

    100 entries

--- Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
@@ -29,15 +29,15 @@ class Checklist10ViewController: UITableViewController {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
     
     if let label = cell.viewWithTag(1000) as! UILabel? {
-      if indexPath.row == 0 {
-        label.text = "Walk the dog"
-      } else if indexPath.row == 1 {
+      if indexPath.row % 5 == 0 {
+        label.text % 5 = "Walk the dog"
+      } else if indexPath.row % 5 == 1 {
         label.text = "Brush my teeth"
-      } else if indexPath.row == 2 {
+      } else if indexPath.row % 5 == 2 {
         label.text = "Learn iOS development"
-      } else if indexPath.row == 3 {
+      } else if indexPath.row % 5 == 3 {
         label.text = "Soccer practice"
-      } else if indexPath.row == 4 {
+      } else if indexPath.row % 5 == 4 {
         label.text = "Eat ice cream"
       }
     }
