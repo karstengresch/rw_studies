eec67cf495fd21fb34d7519ff90c8b4f17374b9e
Karsten Gresch <git-2015@by-doing.org>
2016-10-21 22:38:05 +0200

    Checkmark switching w/ status. Clumsy.

--- Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
@@ -62,7 +62,24 @@ class Checklist10ViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
     if let cell = tableView.cellForRow(at: indexPath) {
-      if cell.accessoryType == .none {
+      var isChecked = false
+      if indexPath.row == 0 {
+        row0checked = !row0checked
+        isChecked = row0checked
+      } else if indexPath.row == 1 {
+        row1checked = !row1checked
+        isChecked = row1checked
+      } else if indexPath.row == 2 {
+        row2checked = !row2checked
+        isChecked = row2checked
+      } else if indexPath.row == 3 {
+        row3checked = !row3checked
+        isChecked = row3checked
+      } else if indexPath.row == 4 {
+        row4checked = !row4checked
+        isChecked = row4checked
+      }
+      if isChecked {
         cell.accessoryType = .checkmark
       } else {
         cell.accessoryType = .none
