64bfcd2cdc74ef5f10fd5254e9e56191508530e4
Karsten Gresch <git-2015@by-doing.org>
2016-10-25 17:18:57 +0200

    Changing the default values

--- Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/Checklist10ViewController.swift
@@ -17,10 +17,10 @@ class Checklist10ViewController: UITableViewController {
   var row4text = "Eat ice cream"
   
   var row0checked = false
-  var row1checked = false
-  var row2checked = false
+  var row1checked = true
+  var row2checked = true
   var row3checked = false
-  var row4checked = false
+  var row4checked = true
 
   override func viewDidLoad() {
     super.viewDidLoad()
