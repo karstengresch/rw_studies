4b6329b869b1954489b70b43b38cbee98cf2f6e0
Karsten Gresch <git-2015@by-doing.org>
2016-11-20 22:24:55 +0100

    Added VC Swift file for new TVC

---
+++ Apprentice/Checklists10/Checklists10/Checklists10/AddChecklist10ItemViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddChecklist10ItemViewController.swift
+//  Checklists10
+//
+//  Created by Karsten Gresch on 20.11.16.
+//  Copyright © 2016 Closure One. All rights reserved.
+//
+
+import Foundation
