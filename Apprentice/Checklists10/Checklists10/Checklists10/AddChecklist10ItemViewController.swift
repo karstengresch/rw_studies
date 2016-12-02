7217007ae44c0cf9e133402c7912437d31dd090b
Karsten Gresch <git-2015@by-doing.org>
2016-12-02 20:26:51 +0100

    Segue func skeleton fix

--- Apprentice/Checklists10/Checklists10/Checklists10/AddChecklist10ItemViewController.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/AddChecklist10ItemViewController.swift
@@ -41,8 +41,8 @@ class AddChecklist10ItemViewController: UITableViewController, UITextFieldDelega
     delegate?.addChecklist10ItemViewController(self, didFinishingAdding: checklist10Item)
   }
   
-  func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+  
   }
   
   
