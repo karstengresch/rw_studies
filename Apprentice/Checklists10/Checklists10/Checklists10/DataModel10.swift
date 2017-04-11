2ccf65fe39915e089847cc7b2e4ea2828d9a562a
Karsten Gresch <git-2015@by-doing.org>
2017-04-11 19:07:35 +0200

    Put the persistence to DataModel which's name should be different e.g. PersistenceManager

--- Apprentice/Checklists10/Checklists10/Checklists10/DataModel10.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/DataModel10.swift
@@ -13,4 +13,34 @@ class DataModel10 {
   
   
   
+  
+  // Utility methods
+  func documentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+  }
+  
+  
+  func dataFilePath() -> URL {
+    return documentsDirectory().appendingPathComponent("Checklist10.plist")
+  }
+  
+  
+  func saveChecklist10s() {
+    let data = NSMutableData()
+    let archiver = NSKeyedArchiver(forWritingWith: data)
+    archiver.encode(checklist10s, forKey: "Checklist10s")
+    archiver.finishEncoding()
+    data.write(to: dataFilePath(), atomically: true)
+  }
+  
+  func loadChecklist10s() {
+    let path = dataFilePath()
+    if let data = try? Data(contentsOf: path) {
+      let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
+      checklist10s = unarchiver.decodeObject(forKey: "Checklist10s") as! [Checklist10]
+      unarchiver.finishDecoding()
+    }
+  }
+  
 }
