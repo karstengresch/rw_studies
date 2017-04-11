ebc31172fe4f2471cd7fc0610ac82ac6a21b1270
Karsten Gresch <git-2015@by-doing.org>
2017-04-11 19:06:54 +0200

    Cut out the persistence methods

---
+++ Apprentice/Checklists10/Checklists10/Checklists10/DataModel10.swift
@@ -0,0 +1,16 @@
+//
+//  DataModel10.swift
+//  Checklists10
+//
+//  Created by Karsten Gresch on 11.04.17.
+//  Copyright © 2017 Closure One. All rights reserved.
+//
+
+import Foundation
+
+class DataModel10 {
+  var checklist10s = [Checklist10]()
+  
+  
+  
+}
