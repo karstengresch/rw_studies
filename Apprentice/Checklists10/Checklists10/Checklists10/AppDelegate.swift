a872c1ed940b3bdfb9530c98ecf9631831692028
Karsten Gresch <git-2015@by-doing.org>
2017-04-11 19:28:08 +0200

    Passing dataModel10 to AllListsViewController10 in AppDelegate

--- Apprentice/Checklists10/Checklists10/Checklists10/AppDelegate.swift
+++ Apprentice/Checklists10/Checklists10/Checklists10/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     let navigationController = window!.rootViewController as! UINavigationController
     let controller = navigationController.viewControllers[0] as! AllLists10ViewController
-    controller.dataModel10 = 
+    controller.dataModel10 = dataModel10
     return true
   }
 
