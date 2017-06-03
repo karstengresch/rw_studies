9ad5894e5a4d3de47601f0715ce0130875058eb3
Karsten Gresch <git-2015@by-doing.org>
2017-06-03 23:36:03 +0200

    Adding outlet for table view and add name action

--- CoreData/HitList/HitList/ViewController.swift
+++ CoreData/HitList/HitList/ViewController.swift
@@ -15,9 +15,11 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBOutlet weak var tableView: UITableView!
+  
+  
+  
+  @IBAction func addName(_ sender: UIBarButtonItem) {
   }
 
 
