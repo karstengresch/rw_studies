6f8c85f3f724c8a1bd47ade2525ecb5c659417c1
Karsten Gresch <git-2015@by-doing.org>
2017-06-06 23:49:30 +0200

    Added the managed subclasses

---
+++ CoreData/Bow Ties/Bowtie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Karsten Gresch on 06.06.17.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Bowtie)
+public class Bowtie: NSManagedObject {
+
+}
---
+++ CoreData/Bow Ties/Bowtie+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Bowtie+CoreDataProperties.swift
+//  Bow Ties
+//
+//  Created by Karsten Gresch on 06.06.17.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Bowtie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Bowtie> {
+        return NSFetchRequest<Bowtie>(entityName: "Bowtie")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var isFavorite: Bool
+    @NSManaged public var lastWorn: NSDate?
+    @NSManaged public var searchKey: String?
+    @NSManaged public var timesWorn: Int32
+    @NSManaged public var photoData: NSData?
+    @NSManaged public var tintColor: NSObject?
+
+}
