5402e479f18533e039648b9ab3b16736fcba23ac
Karsten Gresch <git-2015@by-doing.org>
2017-06-26 19:57:07 +0200

    Added the managed entities. Now broken

---
+++ CoreData/Dog Walk/Dog+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Dog+CoreDataClass.swift
+//  Dog Walk
+//
+//  Created by Karsten Gresch on 26.06.17.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Dog)
+public class Dog: NSManagedObject {
+
+}
---
+++ CoreData/Dog Walk/Walk+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Walk+CoreDataProperties.swift
+//  Dog Walk
+//
+//  Created by Karsten Gresch on 26.06.17.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Walk {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Walk> {
+        return NSFetchRequest<Walk>(entityName: "Walk")
+    }
+
+    @NSManaged public var date: NSDate?
+    @NSManaged public var walks: Dog?
+
+}
