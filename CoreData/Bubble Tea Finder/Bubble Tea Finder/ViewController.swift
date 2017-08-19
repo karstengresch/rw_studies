07fcf50fc6d42e42c6d6e642193c14e7b3b7a23c
Karsten Gresch <git-2015@by-doing.org>
2017-08-19 19:44:46 +0200

    making VC the filterVC delegate

--- CoreData/Bubble Tea Finder/Bubble Tea Finder/ViewController.swift
+++ CoreData/Bubble Tea Finder/Bubble Tea Finder/ViewController.swift
@@ -59,6 +59,7 @@ class ViewController: UIViewController {
       return
     }
     filterViewController.coreDataStack = coreDataStack
+    filterViewController.filterViewControllerDelegate = self
   }
 }
 
